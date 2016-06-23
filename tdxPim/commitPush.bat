4830600086d993bc3410cb90c12d40f9f5746daf
teddy <teddy@fic-sh.com.cn>
2016-06-23 12:36:52 +0800

    commit

--- tdxPim/commitPush.bat
+++ tdxPim/commitPush.bat
@@ -1,5 +1,7 @@
-xcopy "D:\Program Files (x86)\通达信\T0002\diary" "T0002\"  /y /e /h
-xcopy "D:\Program Files (x86)\通达信\T0002\invest" "T0002\"  /y /e /h
-xcopy "D:\Program Files (x86)\通达信\T0002\note" "T0002\"  /y /e /h
+xcopy "D:\Program Files (x86)\通达信\T0002\note" "T0002\note\" /y /e /h
+xcopy "D:\Program Files (x86)\通达信\T0002\diary" "T0002\diary\" /y /e /h
+xcopy "D:\Program Files (x86)\通达信\T0002\invest" "T0002\invest\" /y /e /h
+
+git add -A
 git commit -m"commit"
 git push
