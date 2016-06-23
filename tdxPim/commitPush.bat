758c96c44bcb3d80502c9b596afbbe87574949f9
teddy <teddy@fic-sh.com.cn>
2016-06-23 12:21:57 +0800

    first time

---
+++ tdxPim/commitPush.bat
@@ -0,0 +1,5 @@
+xcopy "D:\Program Files (x86)\通达信\T0002\diary" "T0002\"  /y /e /h
+xcopy "D:\Program Files (x86)\通达信\T0002\invest" "T0002\"  /y /e /h
+xcopy "D:\Program Files (x86)\通达信\T0002\note" "T0002\"  /y /e /h
+git commit -m"commit"
+git push
---
+++ tdxPim/pull.bat
@@ -0,0 +1,2 @@
+git pull
+xcopy "T0002\*" "D:\Program Files (x86)\Õ®¥Ô–≈\T0002\" /y /e /h
